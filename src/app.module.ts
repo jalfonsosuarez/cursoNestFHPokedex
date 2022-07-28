3c170b9fe9c50d81639523fbf72a3cc03c57a3f0
= <=>
2022-07-28 19:34:26 +0200

    POST recibir y validad la data.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
